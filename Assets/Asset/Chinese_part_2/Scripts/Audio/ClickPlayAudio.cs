8030dcc92552e6a5a2f809564205bea72a4c9160
AvinashGameDev-DL <117093224+GameDev-DL@users.noreply.github.com>
2023-03-13 19:23:03 +0530

    Overview and Final Activity Commit
    Done with the overview slide and drag and drop of greatwall activity.

---
+++ Assets/Asset/Chinese_part_2/Scripts/Audio/ClickPlayAudio.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickPlayAudio : MonoBehaviour
+{
+    public AudioClip audioclip;
+
+    private void Start()
+    {
+        SoundManager.Instance.EffectsSource.Stop();
+    }
+
+    public void Onclick()
+    {
+        SoundManager.Instance.EffectsSource.Play();
+    }
+}
---
+++ Assets/Asset/Chinese_part_2/Scripts/GreatWall/Piece_drag.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Piece_drag : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    Vector2 mousePos;
+    Camera mainCam;
+    public CanvasGroup canvasGroup;
+
+    public Transform originalParent = null;
+
+
+    private void Awake()
+    {
+        mainCam = Camera.main;
+        canvasGroup = GetComponent<CanvasGroup>();
+        originalParent = this.transform.parent;
+
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+        this.transform.SetParent(originalParent.parent.parent.parent);
+        canvasGroup.alpha = .5f;
+        canvasGroup.blocksRaycasts = false;
+
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        this.transform.position = mousePos;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        canvasGroup.alpha = 1f;
+        this.transform.SetParent(originalParent);
+        canvasGroup.blocksRaycasts = true;
+
+
+    }
+
+
+}
